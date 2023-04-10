c7f6db40241d061716b12ae86775babb5f2d8ab3
Daniel Schmid <daniel.schmid@mailbox.org>
2023-04-10 13:37:38 +0200

    feat: add search page and function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: 'file'
+  },
+  integrations: [pagefind()]
+})
