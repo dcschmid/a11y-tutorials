2425cd7f43c51a47780bf0ae03b1689640519ae7
Daniel Schmid <daniel.schmid@mailbox.org>
2023-04-10 17:13:10 +0200

    feat: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config'
+import AutoImport from 'astro-auto-import'
+import mdx from '@astrojs/mdx'
 import pagefind from 'astro-pagefind'
+
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks'
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     format: 'file'
   },
-  integrations: [pagefind()]
+  integrations: [
+    AutoImport({
+      imports: [mdxCodeBlockAutoImport('./src/components/CodeBlock.astro')]
+    }),
+    MDXCodeBlocks(),
+    mdx(),
+    pagefind()
+  ]
 })
