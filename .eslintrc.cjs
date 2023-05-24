d2d16fcd0e0bb815d3d616e0392330e1d81545f7
Daniel Schmid <daniel.schmid@mailbox.org>
2023-05-24 16:46:45 +0200

    feat: edited chapter 1 and 2 and upgrade deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,32 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['plugin:astro/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    ecmaVersion: 'latest'
-  },
-  overrides: [
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro']
-      },
-      rules: {
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
-      }
-    }
-  ]
+    extends: ['plugin:astro/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+    },
+    overrides: [
+        {
+            files: ['*.astro'],
+            parser: 'astro-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro']
+            },
+            rules: {
+                // override/add rules settings here, such as:
+                // "astro/no-set-html-directive": "error"
+            }
+        },
+        {
+            files: ['*.md', '*.mdx'],
+            parser: 'eslint-plugin-markdownlint/parser',
+            extends: ['plugin:markdownlint/recommended'],
+            rules: {
+                'markdownlint/md033': ['off']
+            }
+        }
+    ]
 }
